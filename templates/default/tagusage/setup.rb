bd2db3de10765d3998f5062cf6a789eec3141d4b
Franklin Webber <franklin.webber@gmail.com>
2010-10-01 01:57:22 -0700

    Updated all the full lists to include the file and line number. Updated the format of the tags page to be more compact and visually appealing Added additional information that shows the total of features and scenarios that are marked with a tag.

--- templates/default/tagusage/setup.rb
+++ templates/default/tagusage/setup.rb
@@ -2,4 +2,18 @@ def init
   super
   sections.push :tagusage
   @tags = object
+  
+  @tag_usage = { :features => 0,
+    :direct_scenarios => 0,
+    :indirect_scenarios => 0 }
+  
+  @tags.tags.each do |tag|
+    if tag.scenario
+      @tag_usage[:direct_scenarios] += 1 
+    else
+      @tag_usage[:features] += 1
+      @tag_usage[:indirect_scenarios] += tag.feature.scenarios.length
+    end
+  end
+  
 end
