3d2b1efdbd0af558db1cade592ea4cd0644b04ef
Franklin Webber <franklin.webber@gmail.com>
2011-02-06 16:41:44 -0800

    Revert "CITY becoming YARD for YARD plugin compliance"
    This reverts commit 6a09f3c72f86199b9b42bd8878c1a93c22a42bd3.

--- lib/city.rb
+++ lib/city.rb
@@ -4,7 +4,7 @@ require 'gherkin/formatter/tag_count_formatter'
 
 
 module CucumberInTheYARD
-  VERSION = '2.0.0' unless defined?(CucumberInTheYARD::VERSION)
+  VERSION = '1.7.9' unless defined?(CucumberInTheYARD::VERSION)
 end
 
 require File.dirname(__FILE__) + "/yard/code_objects/cucumber/base.rb"
@@ -27,10 +27,9 @@ require File.dirname(__FILE__) + "/yard/parser/cucumber/feature.rb"
 require File.dirname(__FILE__) + "/yard/handlers/cucumber/base.rb"
 require File.dirname(__FILE__) + "/yard/handlers/cucumber/feature_handler.rb"
 
-if RUBY19
-  require File.dirname(__FILE__) + "/yard/handlers/step_definition_handler.rb"
-  require File.dirname(__FILE__) + "/yard/handlers/step_transform_handler.rb"
-end
+require File.dirname(__FILE__) + "/yard/handlers/step_definition_handler.rb"
+require File.dirname(__FILE__) + "/yard/handlers/step_transform_handler.rb"
+
 
 require File.dirname(__FILE__) + "/yard/handlers/legacy/step_definition_handler.rb"
 require File.dirname(__FILE__) + "/yard/handlers/legacy/step_transform_handler.rb"
@@ -41,6 +40,4 @@ require File.dirname(__FILE__) + "/yard/templates/helpers/base_helper.rb"
 require File.dirname(__FILE__) + "/yard/server/commands/list_command.rb"
 require File.dirname(__FILE__) + "/yard/server/router.rb"
 
-#require File.dirname(__FILE__) + "/yard/rake/city_task.rb"
-
-YARD::Templates::Engine.register_template_path File.dirname(__FILE__) + '/templates'
+require File.dirname(__FILE__) + "/yard/rake/city_task.rb"
