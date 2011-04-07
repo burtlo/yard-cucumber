071b2d1a0d994c9d56ceace13a78068895153ab7
Franklin Webber <franklin.webber@gmail.com>
2011-04-06 22:51:33 -0700

    Missing Features/Tags generate pages
     + When yard-cucumber is installed the additional menu items are installed
       for features and tags but no pages were being generated. Updated to
       generate nearly empty lists for features and tags instead of no pages

--- lib/templates/default/fulldoc/html/setup.rb
+++ lib/templates/default/fulldoc/html/setup.rb
@@ -2,47 +2,66 @@ include YARD::Templates::Helpers::HtmlHelper
 
 def init
   super
+  
+  # Additional javascript that power the additional menus, collapsing, etc.
   asset("js/cucumber.js",file("js/cucumber.js",true))
+
+  #
+  # Generate pages for each feature, with the 'feature' template and then 
+  # generate the page which is the full list of features
+  #
 
   @features = Registry.all(:feature)
 
   if @features
     @features.each {|feature| serialize(feature) } 
-    generate_full_list @features.sort {|x,y| x.value.to_s <=> y.value.to_s }
+    generate_full_list @features.sort {|x,y| x.value.to_s <=> y.value.to_s }, :feature
   end
+  
+  #
+  # Generate pages for each tag, with the 'tag' template and then generate the
+  # page which is the full list of tags. Tags are ordered in descending order
+  # by the size of how many scenarios that the affect
+  #
   
   @tags = Registry.all(:tag)
 
   if @tags
     @tags.each {|tag| serialize(tag) }
-    generate_full_list @tags.sort {|x,y| y.all_scenarios.size <=> x.all_scenarios.size }
+    generate_full_list @tags.sort {|x,y| y.all_scenarios.size <=> x.all_scenarios.size }, :tag
   end
 
-  feature_directories = YARD::CodeObjects::Cucumber::CUCUMBER_NAMESPACE.children.find_all {|child| child.is_a?(YARD::CodeObjects::Cucumber::FeatureDirectory) }
-
-
+  # Generates the requirements splash page with the 'requirements' template
   serialize(YARD::CodeObjects::Cucumber::CUCUMBER_NAMESPACE)
+
+  # Generates a page for step definitions and step transforms with the 'steptransformers' template
   serialize(YARD::CodeObjects::Cucumber::CUCUMBER_STEPTRANSFORM_NAMESPACE)
+  
+  # Generates the tags page with the 'featuretags' template
   serialize(YARD::CodeObjects::Cucumber::CUCUMBER_TAG_NAMESPACE)
+  
+  # Generate pages for each of the directories with features with the 'featuredirectory' template
+  feature_directories = YARD::CodeObjects::Cucumber::CUCUMBER_NAMESPACE.children.find_all {|child| child.is_a?(YARD::CodeObjects::Cucumber::FeatureDirectory) }
   serialize_feature_directories(feature_directories)
 
 end
 
-
-def generate_full_list(objects,friendly_name=nil)
-  if !objects.empty?
+#
+# Generate a full_list page of the specified objects with the specified type.
+#
+def generate_full_list(objects,list_type,friendly_name=nil)
     @items = objects
-    @list_type = "#{objects.first.type.to_s}s"
-    @list_title = "#{friendly_name || objects.first.type.to_s.capitalize} List"
+    @list_type = "#{list_type}s"
+    @list_title = "#{friendly_name || list_type.to_s.capitalize} List"
     @list_class = "class"
-    asset("#{objects.first.type}_list.html",erb(:full_list))
-  else
-    log.warn "Full List: Failed to create a list because the objects array is empty."
-  end
+    asset("#{list_type}_list.html",erb(:full_list))
 end
 
 #
-# Remove CUCUMBER namespace from the class list
+# The existing 'Class List' search field would normally contain the Cucumber
+# Namespace object that has been added. Here we call the class_list method 
+# that is contained in the YARD template and we remove the namespace. Returning
+# it when we are done. 
 #
 def class_list(root = Registry.root)
   root.instance_eval { children.delete YARD::CodeObjects::Cucumber::CUCUMBER_NAMESPACE } if root == Registry.root
@@ -51,6 +70,11 @@ def class_list(root = Registry.root)
   out
 end
 
+#
+# Generate a page for each Feature Directory. This is called recursively to 
+# ensure that all feature directories contained as children are rendered to
+# pages.
+#
 def serialize_feature_directories(namespaces)
   namespaces.each do |namespace|
     Templates::Engine.with_serializer(namespace, options[:serializer]) do
@@ -61,10 +85,18 @@ def serialize_feature_directories(namespaces)
   end
 end
 
+#
+# Generate a link to the 'All Features' in the features_list.html
+#
+# When there are no feature directories or multiple top-level feature directories 
+# then we want to link to the 'Requirements' page
+#
+# When there are is just one feature directory then we want to link to that directory
+#
 def all_features_link
   root_feature_directories = YARD::CodeObjects::Cucumber::CUCUMBER_NAMESPACE.children.find_all {|child| child.is_a?(YARD::CodeObjects::Cucumber::FeatureDirectory)}
 
-  if root_feature_directories.length > 1
+  if root_feature_directories.length == 0 || root_feature_directories.length > 1
     linkify YARD::CodeObjects::Cucumber::CUCUMBER_NAMESPACE, "All Features"
   else
     linkify root_feature_directories.first, "All Features"
