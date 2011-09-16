0bbe0251c3d4c622fa081523d6245854f809fe93
Franklin Webber <franklin.webber@gmail.com>
2011-09-16 16:13:24 -0700

    JRuby FIX: The Gherkin lexer/parser language object is not used
      This was causing an issue when marshalling the feature before generating
      the output in YARD.

--- lib/yard/parser/cucumber/feature.rb
+++ lib/yard/parser/cucumber/feature.rb
@@ -38,12 +38,15 @@ module YARD::Parser::Cucumber
         @parser.parse(@source, @file, 0)
         @feature = @builder.ast
         return nil if @feature.nil? # Nothing matched
-        @feature.language = @parser.i18n_language
+        
+        # The parser used the following keywords when parsing the feature
+        # @feature.language = @parser.i18n_language.get_code_keywords.map {|word| word }
+        
       rescue Gherkin::Lexer::LexingError, Gherkin::Parser::ParseError => e
         e.message.insert(0, "#{@file}: ")
         raise e
       end
-
+      
       self
     end
 
