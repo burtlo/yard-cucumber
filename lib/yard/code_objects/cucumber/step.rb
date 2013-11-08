1bc22c4e4696df32973d1fad0584f4e8daaea30b
Erran Carey <me@errancarey.com>
2013-11-08 14:49:55 -0600

    Stop displaying duplicate usages of step definitions

--- lib/yard/code_objects/cucumber/step.rb
+++ lib/yard/code_objects/cucumber/step.rb
@@ -3,16 +3,16 @@
 module YARD::CodeObjects::Cucumber
 
   class Step < Base
-    
+
     attr_accessor :comments, :definition, :examples, :keyword, :scenario, :table, :text, :transforms, :value
-    
+
     def initialize(namespace,name)
       super(namespace,name.to_s.strip)
       @comments = @definition = @description = @keyword = @table = @text = @value = nil
       @examples = {}
       @transforms = []
     end
-    
+
     def has_table?
       !@table.nil?
     end
@@ -20,12 +20,15 @@ module YARD::CodeObjects::Cucumber
     def has_text?
       !@text.nil?
     end
-    
+
     def definition=(stepdef)
       @definition = stepdef
-      stepdef.steps << self
+
+      unless stepdef.steps.map(&:files).include?(files)
+        stepdef.steps << self
+      end
     end
-    
+
     def transformed?
       !@transforms.empty?
     end
